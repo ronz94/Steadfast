5f192f6952f41fdd38cd7e871ff177a406d3d574
Abhishek Kumar <kumar.abhishek@techie.com>
2015-08-03 02:30:18 +0530

    Adjust class,main functions,configs,classpath for development
    Summary: Basically adjusted the whole project for development in eclipse,
    included it's classpath local settings. Also changed it's settings and
    class path to work on java SE 1.7.
    
    Squashed commit of the following:
    
    commit 7309cd8ac2cf9123b4cda40a2a555fc61891f531
    Author: Abhishek Kumar <kumar.abhishek@techie.com>
    Date:   Mon Aug 3 02:24:59 2015 +0530
    
        change default message
    
    commit ff59c4f77ee63c28a001db019583dfb826a7d38e
    Author: Ranit Saha <ranit1.saha@gmail.com>
    Date:   Sun Aug 2 01:49:05 2015 +0530
    
        Change main class to Steadfast
    
    commit ef9f91e5a80382d9022540702ac51ef72f231941
    Author: Abhishek Kumar <kumar.abhishek@techie.com>
    Date:   Sun Aug 2 00:41:33 2015 +0530
    
        use java SE 1.7
    
    commit c15dc43e81d14b2a2de31944dcb47ec8b0fc9b4a
    Author: Ranit Saha <ranit1.saha@gmail.com>
    Date:   Sun Aug 2 00:15:35 2015 +0530
    
        Add settings dir
    
    commit bf52e2c44adabf95d14e0300cb529c311c6a9f03
    Author: Abhishek Kumar <kumar.abhishek@techie.com>
    Date:   Sun Aug 2 00:11:12 2015 +0530
    
        add local settings
    
    commit 4fa7653472138735e5d791cf77e6448124af280d
    Author: Ranit Saha <ranit1.saha@gmail.com>
    Date:   Sat Aug 1 23:48:46 2015 +0530
    
        Add classpath and project config
    
    commit d01f148cf8b260295e3c389a6d32a5cfd221e310
    Author: Abhishek Kumar <kumar.abhishek@techie.com>
    Date:   Sat Aug 1 23:38:42 2015 +0530
    
        included eclipse settings

--- src/Steadfast.java
+++ src/Steadfast.java
@@ -1,10 +1,10 @@
 
-public class HelloWorld {
+public class Steadfast {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World!");
+		System.out.println("Running SteadFast !");
 
 	}
 
