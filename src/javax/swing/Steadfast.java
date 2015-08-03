0103fd584836e28a10b22ccf4a00f74344db8c3f
Ranit Saha <ranit1.saha@gmail.com>
2015-08-04 00:35:30 +0530

    Create GUI window

---
+++ src/javax/swing/Steadfast.java
@@ -0,0 +1,29 @@
+package javax.swing;
+
+public class Steadfast extends JFrame {
+	/**
+	+	 * 
+	+	 */
+		private static final long serialVersionUID = 1L;
+	
+		public Steadfast() {
+			// Making sure we have a nice window decorations
+			setDefaultLookAndFeelDecorated(true);
+			//creating and setting up windows
+			setTitle("Hi swing !!");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			// Adding label with the text to be displayed.
+			JLabel label =  new JLabel("GUI window created !!");
+			getContentPane().add(label);
+			//displaying window
+			pack();
+			setVisible(true);
+		}
+		
+		public static void main(String[]args){
+			@SuppressWarnings("unused")
+			Steadfast justswing = new Steadfast();
+			
+		}
+	
+}
