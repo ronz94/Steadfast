369cf5d8e218e6bb33d1549bb55a41f83508971e
Mr.Kumar <kumar.abhishek@techie.com>
2015-08-04 07:46:39 +0530

    set default look and feel

--- src/Steadfast.java
+++ src/Steadfast.java
@@ -14,6 +14,7 @@ public class Steadfast extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public static final void main(String[] args) throws Exception {
+		setDefaultLookAndFeelDecorated(true);
 		String website = "https://github.com/ronz94/Steadfast/archive/basicgui.zip";
 		String itemname = "basicgui.zip";
 		JFrame dhanja = new JFrame();
